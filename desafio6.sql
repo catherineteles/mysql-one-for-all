7c0b1a018581d16881baa147cb475ec66508b5f4
cathe <cacateles12@gmail.com>
2022-06-03 16:27:38 -0300

    Getting budget

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+  MIN(p.plan_value) AS faturamento_minimo,
+  MAX(p.plan_value) AS faturamento_maximo,
+  ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
+  SUM(p.plan_value) AS faturamento_total
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.plans AS p
+ON u.plan_id = p.plan_id;
