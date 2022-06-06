54cdb2c17314881b270f62b4ed2ed4557dfe7d54
cathe <cacateles12@gmail.com>
2022-06-06 10:57:06 -0300

    Select songs and times played in personal or free plans

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT
+  s.song_name AS nome,
+  COUNT(s.song_name) AS reproduçoes
+FROM SpotifyClone.songs AS s
+INNER JOIN SpotifyClone.history AS h
+ON s.song_id = h.song_id
+INNER JOIN SpotifyClone.users AS u
+ON h.user_id = u.id
+WHERE plan_id = 1 OR plan_id = 4
+GROUP BY s.song_name;
